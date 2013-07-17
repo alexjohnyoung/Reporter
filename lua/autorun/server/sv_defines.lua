f79f448ec77c77ca74d8563a53b94e3d0e3fa074
tyguy550 <alexjohnyoung@gmail.com>
2013-07-17 08:33:09 +0100

    Made ConVars show up clientside in console

--- lua/autorun/server/sv_defines.lua
+++ lua/autorun/server/sv_defines.lua
@@ -1,9 +1,9 @@
 tagre = "reporter" --The tag, don't bother changing it.
-PropKillReporter = CreateConVar(tagre.."_propkillreporter", 1, true, false) --The ConVar to enable or disable the anti-propkill
-PropKillWarnings = CreateConVar(tagre.."_propkillwarnings", 3, true, false) --If the anti-propkill is enabled, this is how much warnings you will have before getting kicked.
-BombPropsBlocked = CreateConVar(tagre.."_blockbombs", 1, true, false) --Enable/disable to block bombs
-DamageLog = CreateConVar(tagre.."_damagelogcons", 1, true, false) --Enable/disable to log damage
-DamageLogSaving = CreateConVar(tagre.."_damagelogsaving", 1, true, false) --If logging damage, enable this to save it in a file.
+PropKillReporter = CreateClientConVar(tagre.."_propkillreporter", 1, true, false) --The ConVar to enable or disable the anti-propkill
+PropKillWarnings = CreateClientConVar(tagre.."_propkillwarnings", 3, true, false) --If the anti-propkill is enabled, this is how much warnings you will have before getting kicked.
+BombPropsBlocked = CreateClientConVar(tagre.."_blockbombs", 1, true, false) --Enable/disable to block bombs
+DamageLog = CreateClientConVar(tagre.."_damagelogcons", 1, true, false) --Enable/disable to log damage
+DamageLogSaving = CreateClientConVar(tagre.."_damagelogsaving", 1, true, false) --If logging damage, enable this to save it in a file.
 
 AdvertReporter = "This server is running Tyguy's Reporter System" --Don't change this please.
 
